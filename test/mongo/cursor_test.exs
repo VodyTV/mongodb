122db3ce931e325b900e6b2937d7665df51c6b9e
Everett Griffiths <everett@vody.com>
2020-01-06 14:36:44 -0800

    Adds test for limit, skip, and sort

--- test/mongo/cursor_test.exs
+++ test/mongo/cursor_test.exs
@@ -22,4 +22,28 @@ defmodule Mongo.CursorTest do
     assert [%{"foo" => 42}, %{"foo" => 42}] =
              c.pid |> Mongo.find(coll, %{}, limit: 2) |> Enum.to_list()
   end
+
+  test "limit", c do
+    coll = unique_name()
+
+    docs = [%{name: "a"}, %{name: "b"}, %{name: "c"}]
+    assert {:ok, _} = Mongo.insert_many(c.pid, coll, docs)
+    assert [%{"name" => "a"}, %{"name" => "b"}] = Mongo.find(c.pid, coll, %{}, limit: 2) |> Enum.to_list
+  end
+
+  test "skip", c do
+    coll = unique_name()
+
+    docs = [%{name: "a"}, %{name: "b"}, %{name: "c"}]
+    assert {:ok, _} = Mongo.insert_many(c.pid, coll, docs)
+    assert [%{"name" => "b"}, %{"name" => "c"}] = Mongo.find(c.pid, coll, %{}, limit: 2, skip: 1) |> Enum.to_list
+  end
+
+  test "sort", c do
+    coll = unique_name()
+
+    docs = [%{name: "b"}, %{name: "a"}, %{name: "c"}]
+    assert {:ok, _} = Mongo.insert_many(c.pid, coll, docs)
+    assert [%{"name" => "c"}, %{"name" => "b"}, %{"name" => "a"}] = Mongo.find(c.pid, coll, %{}, [sort: %{name: -1}]) |> Enum.to_list
+  end
 end
